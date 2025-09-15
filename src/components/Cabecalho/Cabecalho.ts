78ed2cc9c1525d95d33c994a4c8be54fbff71b35
Jonas Maia <jonasgmaia2@gmail.com>
2025-09-15 20:25:51 -0300

    Update and rename Cabecalho.module.css to Cabecalho.ts

---
+++ src/components/Cabecalho/Cabecalho.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import EstiloGlobal from './global.ts'
+
+export const cabecalho = styled.header
+`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
