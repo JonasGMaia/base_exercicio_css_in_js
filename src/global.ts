75f45aaad699930fd8898102dcc426de13c29e3a
Jonas Maia <jonasgmaia2@gmail.com>
2025-09-15 15:53:35 -0300

    Update global.ts

--- src/global.ts
+++ src/global.ts
@@ -20,16 +20,19 @@ body {
   padding-bottom: 120px;
 }
 
-.container {
+}
+`
+export default EstiloGlobal
+export const Container = styled.div
+
+`
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
-}
+
 
 @media (max-width: 1024px) {
   .container {
     max-width: 80%;
   }
-}
-`
-export default EstiloGlobal
+  `
